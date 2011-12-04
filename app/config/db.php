3c1fe3941bb4304f0425b24fd918a1b854922c1d
Troy <troytft@gmail.com>
2011-12-04 17:25:38 +0400

    add db config

---
+++ app/config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'connectionString' => 'mysql:host=localhost;dbname=pyha',
+    'emulatePrepare' => true,
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => 'pyha_',
+    'enableProfiling' => true,
+    'enableParamLogging' => true,
+);
